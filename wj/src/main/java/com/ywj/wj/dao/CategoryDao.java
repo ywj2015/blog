57dd879ac39f2d62663efd9696949e1190108215
ywj2015 <ywjfighting@163.com>
2019-09-17 10:42:49 +0800

    delete

--- wj/src/main/java/com/ywj/wj/dao/CategoryDao.java
+++
@@ -1,8 +0,0 @@
-package com.ywj.wj.dao;
-
-import com.ywj.wj.pojo.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryDao extends JpaRepository<Category, Integer> {
-
-}
